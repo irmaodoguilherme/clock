a1ceaf3e1cf99a9a77f32f84e90f4ee34cb71566
irmaodoguilherme <airesguilherme83@gmail.com>
2024-10-02 19:35:27 -0300

    added license and readme also switched css for center alignment

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const clockContainer = document.querySelector('[data-js="clock-container"]')
-
 const oneSecond = 1000
 
 const concatenateZero = timeUnit => timeUnit < 10 ? `0${timeUnit}` : timeUnit
@@ -14,12 +13,13 @@ const getTime = () => {
   return [hours, minutes, seconds]
 }
 
-const getClockHTML = ([hours, minutes, seconds]) => {
-  const clockHTML = `
-    <span class="bg-opacity-75 txt-sh txt-stk font-monospace fst-italic bg-info rounded fs-5r px-3">${hours}</span>:
-    <span class="bg-opacity-75 txt-sh txt-stk font-monospace fst-italic bg-info rounded fs-5r px-3">${minutes}</span>:
-    <span class="bg-opacity-75 txt-sh txt-stk font-monospace fst-italic bg-info rounded fs-5r px-3">${seconds}</span>
-  `
+const getClockHTML = time => {
+  const clockHTML = time.reduce((acc, timeUnit, index) => {
+    const normalTimeUnit =
+      `${acc}<span class="bg-opacity-75 txt-sh txt-stk font-monospace fst-italic bg-info rounded fs-5r px-3">${timeUnit}</span>`
+
+    return index === 0 || index == 1 ? normalTimeUnit + ':' : normalTimeUnit
+  }, ``)
 
   return clockHTML
 }
---
+++ readme.md
@@ -0,0 +1,17 @@
+## Sumário
+
+- [Introdução](#introdução)
+- [Como acessar](#como-acessar)
+- [Suporte](#suporte)
+
+**<h1 id="introdução">Relógio</h1>**
+
+Exibe o horário atual encontrado na sua máquina
+
+<h2 id="como-acessar">Como acessar</h2>
+
+Você pode acessá-lo [aqui](clock-gal.netlify.app/) ou através do link na seção 'About'.
+
+<h2 id="suporte">Suporte</h2>
+
+Se você encontrar algum erro durante o uso da aplicação, por favor, abra uma 'Issue'. **Descreva o problema de forma clara e detalhada**, incluindo informações sobre o que aconteceu, quando ocorreu e o que você estava fazendo na ocasião.
